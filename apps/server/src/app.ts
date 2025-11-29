e8233acbc7441ef4e4dece7df3d24ad31824dcbc
joseph0926 <joseph0926@naver.com>
2025-11-30 08:22:40 +0900

    feat: 에러 헨들러 추가

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -2,12 +2,15 @@ import Fastify from "fastify";
 import env from "./plugins/env";
 import cors from "./plugins/cors";
 import health from "./routes/health";
+import errorHandler from "./plugins/error-handler";
 
 export async function buildApp() {
 	const app = Fastify({ logger: true });
 
 	await env(app);
 	await cors(app);
+
+	await errorHandler(app);
 
 	await health(app);
 
